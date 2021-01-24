050f4f5b6f60ebeeb10a2007e1f3d15079c61e81
Deepak Rajan <deepakrajan.grea@gmail.com>
2021-01-25 02:41:02 +0530

    db create-assets

--- flint_terminal/db/db.py
+++ flint_terminal/db/db.py
@@ -1,3 +1,7 @@
 ROOT_DIR = "Y:\\pipeline\\vfx"  # Root directory
 PROJECTS_JSON = "projects.json"  # main project json file
 PROJECT_BASE_ID = 46125  # new project base ID
+
+
+# Asset detail
+ASSETS_JSON = "assets.json"
